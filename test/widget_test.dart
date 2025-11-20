f65858f9f1812578fd5b034c35d54e89c270e7d2
Patiwet-C <patiwet.chiamsakul@gmail.com>
2025-11-20 14:20:13 +0700

    update package name and test pushing github action workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:singpass_poc/main.dart';
+import 'package:kasm_poc_workspace/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
