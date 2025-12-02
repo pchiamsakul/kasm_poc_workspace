3d5fae15202c0f907893406835eb6c08bb547c53
Patiwet-C <patiwet.chiamsakul@gmail.com>
2025-12-02 09:40:03 +0700

    add folder link for image

--- lib/constants/router_name.dart
+++ lib/constants/router_name.dart
@@ -2,4 +2,5 @@ class RouterName {
   RouterName._();
 
   static const String landingPage = "/landing";
+  static const String homePage = "/home";
 }
--- lib/features/landing/pages/landing_page.dart
+++ lib/features/landing/pages/landing_page.dart
@@ -1,6 +1,9 @@
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:kasm_poc_workspace/constants/router_name.dart';
+import 'package:kasm_poc_workspace/di/configure_dependencies.dart';
 import 'package:kasm_poc_workspace/i18n/strings.g.dart';
+import 'package:kasm_poc_workspace/routers/app_navigator.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LandingPage extends StatefulWidget {
@@ -12,6 +15,14 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   int _counter = 0;
+  late final AppNavigator _appNavigator;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _appNavigator = getIt<AppNavigator>();
+  }
 
   void _incrementCounter() async {
     setState(() {
@@ -52,6 +63,22 @@ class _LandingPageState extends State<LandingPage> {
     }
   }
 
+  Widget buttonScreen(String route) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: Column(
+        children: <Widget>[
+          ElevatedButton(
+            onPressed: () => _appNavigator.pushNamed(route),
+            child: Text(route.replaceAll('/', '')),
+          ),
+          SizedBox(height: 8),
+          const Divider(height: 1, color: Colors.grey),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,12 +87,23 @@ class _LandingPageState extends State<LandingPage> {
 
         title: Text(t.app_name),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        alignment: Alignment.center,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
+            ExpansionTile(
+              shape: const Border(),
+              collapsedShape: const Border(),
+              title: Text('Mockup Screen'),
+              children: <Widget>[
+                const Divider(height: 1, color: Colors.grey),
+                buttonScreen(RouterName.homePage),
+              ],
+            ),
           ],
         ),
       ),
